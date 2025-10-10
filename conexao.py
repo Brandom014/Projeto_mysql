ea8fbe917cc69bc99051fe257afee863ea560598
Brandom <brandom.quispe2020@gmail.com>
2025-10-10 11:04:57 -0300

    conexao com db ✔

---
+++ conexao.py
@@ -0,0 +1,26 @@
+import mysql.connector
+from dotenv import load_dotenv
+import os
+# pip install mysql-connector-python
+# pip install dotenv
+
+#Carregar as variaveis de ambiente (.env)
+load_dotenv()
+
+def conectar():
+    try:
+        conexao = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password=os.getenv("DB_PASSWORD"),
+            database="faculdade"
+        )
+        cursor = conexao.cursor()
+        print(f"Conexão estabelecida: {conexao}")
+        return conexao, cursor
+    except Exception as erro:
+        print(f"Erro de conexão: {erro}")
+        return None, None
+    
+conectar()
+
